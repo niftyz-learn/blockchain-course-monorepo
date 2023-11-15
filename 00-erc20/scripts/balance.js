c1642fcb02d94495d728fd7a3869078a99926016
turinglabs <sebastiano.cataudo@gmail.com>
2023-11-15 16:17:42 +0100

    decentralise things

--- 00-erc20/scripts/balance.js
+++ 00-erc20/scripts/balance.js
@@ -4,12 +4,17 @@ const fs = require('fs');
 async function main() {
     const configs = JSON.parse(fs.readFileSync(process.env.CONFIG).toString())
     const ABI = JSON.parse(fs.readFileSync('./artifacts/contracts/' + configs.contract_name + '.sol/' + configs.contract_name + '.json').toString())
+   
     const provider = new ethers.providers.JsonRpcProvider(configs.provider);
+
     let wallet = new ethers.Wallet(configs.owner_key).connect(provider)
+
     const contract = new ethers.Contract(configs.contract_address, ABI.abi, wallet)
 
     console.log("Reading balance for owner:", configs.owner_address)
+    
     const result = await contract.balanceOf(configs.owner_address)
+    
     console.log("Balance is:", result.toString())
     console.log("--")
     // Reading balance for Bob
---
+++ 03-decentralise-things/README.md
@@ -0,0 +1,5 @@
+# Decentralise Things
+
+Simple ready to use Ethereum login with signature.
+
+Use it to authenticate users with Metamask and sign / verify actions in your web2 backend (for example).
